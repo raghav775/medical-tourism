55c1e05747acc7488819eafd094ee9eb2a9d6a7e
raghav775 <74240452+raghav775@users.noreply.github.com>
2026-02-04 22:08:47 +0530

    Create next.config.js with Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add your Next.js configuration options here
+};
+
+module.exports = nextConfig;
